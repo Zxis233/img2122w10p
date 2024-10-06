d5238b58c3d6371f67821c392104efc650f7bfd5
Zyx <esingzhang@stu.xjtu.edu.cn>
2024-10-06 21:20:24 +0800

    Add My Favourite

--- pic2webp.bat
+++ pic2webp.bat
@@ -1,1 +1,1 @@
-for %%a in (*.png *.jpg) do ffmpeg -hwaccel nvdec -i "%%a" -c:v libwebp -q:v 100 "%%~na.webp"
+for %%a in (*.png *.jpg) do ffmpeg -hwaccel nvdec -i "%%a" -c:v libwebp -q:v 60 "new/%%~na.webp"
